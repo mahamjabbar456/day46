1150e16267f70480ac0ae641c4806e0b16874318
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-21 21:33:21 +0500

    i learn about try catch and breakpoint.

---
+++ index.ts
@@ -0,0 +1,20 @@
+// Question 136
+for(let i:number = 1;i<6;i++){
+    console.log(i);
+}
+
+// Question 137
+// try {
+//     // Intentionally cause an error
+//     throw new Error("Something went wrong");
+//   } catch (error) {
+//     console.log(error.message); // Logs the error message
+//   }
+
+// Question 138
+console.log("Before breakpoint");
+// Set a breakpoint on the following line using your browser's developer tools
+console.log(
+  "This line has a breakpoint set on it in the browser's developer tools"
+);
+console.log("After breakpoint");
